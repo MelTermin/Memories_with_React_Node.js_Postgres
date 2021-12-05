d9e397b110559a3e386e202c31a86bdb6d41c78c
Selin Termin <80844457+MelTermin@users.noreply.github.com>
2021-12-05 19:58:15 +0200

    changes

--- client/src/Components/Memories.js
+++ client/src/Components/Memories.js
@@ -1,16 +1,46 @@
 import React, { useState,useContext,useEffect } from 'react'
 import axios from "axios";
 import {MemoryContext} from '../Context/MemoryContext'
+import moment from 'moment';
 
 function Memories() {
   const {details,setDetails}=useContext(MemoryContext)
 
+  // const [singleMemo,setSingleMemo]=useState();
   useEffect (()=> {
     axios.get("http://localhost:5000/memories").then(response=> {
       console.log(response)
       setDetails(response.data)
     })
    },[]);
+  
+//    const {id}=useParams()
+
+
+//    useEffect(()=> {
+//      if(id){
+//        getSingleMemory(id)
+//      }
+//    },[id])
+
+//  const getSingleMemory=async(id) => {
+//   const response= await axios.get(`http://localhost:5000/memory/${id}`)
+//    setSingleMemo(...response.data[0])
+//  }
+
+   
+  //  const handleEdit = (id) =>  {
+  //    console.log(id)
+  //    axios.put(`http://localhost:5000/memory/${id}`,{
+
+  //    }).then(data=> {
+  //     console.log(data)
+  //    })
+  //  }
+
+  const handleEdit= () =>{
+    
+  }
 
   return (
 <div className="memories-container">
@@ -20,8 +50,22 @@ function Memories() {
           return (//dont forget to return it Melissa!!//
           <div  key= {item.id} className="card">
             <img className="image" src={item.selected_file}></img>
-            <p>{item.title}</p>
-            <p>{item.message}</p>
+
+            <div className="overlay">
+              <p>{item.creator}</p>
+              <p>{moment(item.created_at).fromNow()}</p>
+            </div>
+
+            <div className="overlay2">
+              <button onClick= {() => handleEdit(item.id)}>...</button>
+            </div>
+            <small>{item.tags}</small>
+            <strong>{item.title}</strong>
+            <strong>{item.message}</strong>
+            <button>Like{item.like_count}</button>
+            <button>Delete</button>
+           
+            
             
           
           
